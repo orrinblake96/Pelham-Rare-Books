d94ffc14c4586615881e3afe440976e1671a8bc2
OrrinBlake <orrinblake@gmail.com>
2019-12-07 13:53:40 +0000

    add popup for buying products & add items

--- Phlem Rare Books Mobile First/js/scripts.js
+++ Phlem Rare Books Mobile First/js/scripts.js
@@ -1,3 +1,5 @@
+var currentButtonObject = {};
+
 // Handle responsiveness for nav bar toggle
 $(function () {
     $(".toggle").on("click", function () {
@@ -12,14 +14,14 @@ $(function () {
 });
 
 // display form
-function toggleInputForm(){
-	var inputForm = document.getElementById("inputForm");
-	
-	if(inputForm.style.display === "block"){
-		inputForm.style.display = "none";
-	} else {
-		inputForm.style.display = "block";
-	}
+function toggleInputForm() {
+    var inputForm = document.getElementById("inputForm");
+
+    if (inputForm.style.display === "block") {
+        inputForm.style.display = "none";
+    } else {
+        inputForm.style.display = "block";
+    }
 }
 
 /*
@@ -30,7 +32,7 @@ function submitFeedback() {
     var feedbackText = document.getElementById("feedbackText").value;
     var feedbackButton = document.getElementById("feedbackButton");
 
-    if(feedbackText === ""){
+    if (feedbackText === "") {
         document.getElementById("feedbackText").focus();
     } else {
         document.getElementById("feedbackText").style.display = "none";
@@ -66,7 +68,7 @@ var productsArray = [
         genre: "Scientific",
         info: "On the Origin of Species, published on 24 November 1859, is a work of scientific literature by Charles Darwin which is considered to be the foundation of evolutionary biology. Darwin's book introduced the scientific theory that populations evolve over the course of generations through a process of natural selection"
     },
-	{
+    {
         bookName: "Nineteen Eighty-Four",
         bookImage: "https://www.deburcararebooks.com/wp-content/uploads/2019/08/1984-768x1024.jpg",
         price: 425,
@@ -74,7 +76,7 @@ var productsArray = [
         genre: "Fiction",
         info: "Easton Press, Norwalk, Connecticut, 1992. Hardcover. First Edition; First Printing. Fine, Leather Bound. Book accented in 22kt gold. Printed on archival paper with gilded edges. The endsheets are of moire fabric with a silk ribbon page marker. Smyth sewing and concealed muslin joints to ensure the highest quality binding. The novel is set in an imagined future, the year 1984, when much of the world has fallen victim to perpetual war, omnipresent government surveillance, historical negationism and propaganda. Eric Arthur Blair (25 June 1903 – 21 January 1950), better known by his pen name George Orwell, was an English novelist and essayist, journalist and critic, whose work is characterised by lucid prose, awareness of social injustice, opposition to totalitarianism, and outspoken support of democratic socialism."
     },
-	{
+    {
         bookName: "Dracula: STOKER, Bram, True First Edition",
         bookImage: "https://www.deburcararebooks.com/wp-content/uploads/2019/01/Stoker110-687x1024.jpg",
         price: 6500,
@@ -82,7 +84,7 @@ var productsArray = [
         genre: "Fiction",
         info: "Dracula is an 1897 Gothic horror novel by Irish author Bram Stoker. It introduced the character of Count Dracula, and established many conventions of subsequent vampire fantasy"
     },
-	{
+    {
         bookName: "Hamlet",
         bookImage: "https://cdn.usborne.com/catalogue/covers/eng/max_covers/9780746096116.JPG?width=760&quality=60",
         price: 500,
@@ -90,7 +92,7 @@ var productsArray = [
         genre: "Tragedy",
         info: "The Tragedy of Hamlet, Prince of Denmark, often shortened to Hamlet, is a tragedy written by William Shakespeare sometime between 1599 and 1602."
     },
-	{
+    {
         bookName: "Whammond’s Illustrated Guide to Dublin and Wicklow",
         bookImage: "https://www.deburcararebooks.com/wp-content/uploads/2018/12/Whammond018-681x1024.jpg",
         price: 185,
@@ -98,7 +100,7 @@ var productsArray = [
         genre: "Travel",
         info: "WHAMMOND, G.K. Whammond’s Illustrated Guide to Dublin and Wicklow. With an historical sketch of the city. Original Comments, Anecdotes, &c. A Book of Reference for Residents and Visitors. Illustrated with views of the Exhibition Palace, Kingstown Harbour, Sackville Street, Bank of Ireland (Old Irish Parliament Houses), Trinity College, Custom House, St. Patrick’s Cathedral and a map of the city. Dublin: Robert S. Magee, 1869. 12mo. pp. [10], 187, 69 (adverts). Pictorial wrappers. Spine professionally rebacked. A very good copy."
     },
-	{
+    {
         bookName: "Middle English translation of John Bradmore's (d. 1412), surgical-medical treatise, Philomena.",
         bookImage: "https://www.maggs.com/media/3971502/223251_04.jpg",
         price: 250000,
@@ -106,7 +108,7 @@ var productsArray = [
         genre: "Scientific",
         info: "A second, previously unidentified, manuscript of the 1446 Middle English version of the Philomena of John Bradmore, a surgical-medical treatise by one of the most English famous surgeons of the early 15th Century. The only other example is in the British Library MS Harley 1736 and it is largely unpublished. It includes the famous account of how Bradmore saved the life of the young Prince of Wales (Prince Hal, the future King Henry V), after the Battle of Shrewsbury in 1403. It was owned and perhaps written by an early Tudor Barber-Surgeon Charles Whyte (d. 1545) and is presumably one of the two manuscript volumes described in his Will, the other being in the British Library (MS Sloane 776). The manuscript also includes an apparently unique late Middle English “Tretys of mynd” that may be the earliest English work on the subject of the mind and memory."
     },
-	{
+    {
         bookName: "Ulysses",
         bookImage: "https://d3525k1ryd2155.cloudfront.net/h/184/525/601525184.0.x.1.jpg",
         price: 5000,
@@ -114,7 +116,7 @@ var productsArray = [
         genre: "Fiction",
         info: "Ulysses is a modernist novel by Irish writer James Joyce. It was first serialised in parts in the American journal The Little Review from March 1918 to December 1920 and then published in its entirety in Paris by Sylvia Beach on 2 February 1922, Joyce's 40th birthday."
     },
-	{
+    {
         bookName: "Don Quixote",
         bookImage: "https://images.penguinrandomhouse.com/cover/9780679602866",
         price: 15000,
@@ -143,15 +145,15 @@ function getPostDetails() {
         leftColCard.innerHTML += "<h4>" + "Author: </h4>" + "<p>" + productsArray[i].author + "</p> <br>";
         leftColCard.innerHTML += "<h4>" + "Genre: </h4>" + "<p>" + productsArray[i].genre + "</p> <br>";
         leftColCard.innerHTML += "<h4>" + "Description:  </h4>" + "<p>" + productsArray[i].info + "</p> <br>";
-        leftColCard.innerHTML += "<button class='favoriteButton' value='" + i + "' onclick='addToFavorites(this)'>Add to Favorites</button>";
-        leftColCard.innerHTML += " <button class='buyButton' value='" + i + "' onclick='buyBook(this)'>Buy Book</button>";
+        leftColCard.innerHTML += "<button class='favoriteButton' value='" + i + "' onclick='addToFavorites(this)'><i class='fas fa-thumbs-up'></i> Add to Favorites</button>";
+        leftColCard.innerHTML += " <button class='buyButton' value='" + i + "' onclick='openForm(this); this.disabled=true;'><i class='fas fa-credit-card'></i> Buy Book</button>";
     }
 }
 window.onload = getPostDetails();
 
 //add to favoriteBooks array
 function addToFavorites(object) {
-    if (object.innerHTML != "Added"){
+    if (object.innerHTML != "Added") {
         object.innerHTML = "Added";
         favoriteBooks.unshift(
             {
@@ -160,46 +162,45 @@ function addToFavorites(object) {
             }
         );
         updateFavoriteBooks();
-    } 
+    }
 }
 
 //update favorites section on right column
-function updateFavoriteBooks(){
+function updateFavoriteBooks() {
     var rightColCard = document.getElementById("favoriteBooks");
     rightColCard.innerHTML += "<img class='popularPostImages' src='" + favoriteBooks[0].bookImage + "' />" + "<br>";
     rightColCard.innerHTML += "<p>" + favoriteBooks[0].bookName + "</p> <br>";
 
 }
 
-//Buy Book when button is clicked
+//Buy Book when button is clicked, passed global object that contains the current clicked button
 function buyBook(object) {
-    object.innerHTML = "Bought!!";
-    
+    currentButtonObject.innerHTML = "Bought!!";
 }
 
 //Create a book post by getting the user input from the html form
 function createBookPost() {
     var leftColCard = document.getElementById("productsInfo");
-	var postName = document.getElementById("postBookName").value;
-	var postImage = document.getElementById("postBookImage").value;
-	var postPrice = document.getElementById("postBookPrice").value;
-	var postAuthor = document.getElementById("postBookAuthor").value;
-	var postGenre = document.getElementById("postBookGenre").value;
-	var postText = document.getElementById("postText").value;
-	
-	//use regex functions to check name, image url, author name, price  and textarea
-	if (!checkName(postName)) {
-		//if false then focus user back to that input box (used various times)
-		document.getElementById("postBookName").focus();
-	} else if(!checkImage(postImage)) {
-		document.getElementById("postBookImage").focus();
-	} else if (!checkAuthorName(postAuthor)){
-		document.getElementById("postBookAuthor").focus();	
-	} else if (!checkPrice(postPrice)){
-		document.getElementById("postBookPrice").focus();		
-	} else if (!checkName(postText)){
-		document.getElementById("postText").focus();		
-	} else {
+    var postName = document.getElementById("postBookName").value;
+    var postImage = document.getElementById("postBookImage").value;
+    var postPrice = document.getElementById("postBookPrice").value;
+    var postAuthor = document.getElementById("postBookAuthor").value;
+    var postGenre = document.getElementById("postBookGenre").value;
+    var postText = document.getElementById("postText").value;
+
+    //use regex functions to check name, image url, author name, price  and textarea
+    if (!checkName(postName)) {
+        //if false then focus user back to that input box (used various times)
+        document.getElementById("postBookName").focus();
+    } else if (!checkImage(postImage)) {
+        document.getElementById("postBookImage").focus();
+    } else if (!checkAuthorName(postAuthor)) {
+        document.getElementById("postBookAuthor").focus();
+    } else if (!checkPrice(postPrice)) {
+        document.getElementById("postBookPrice").focus();
+    } else if (!checkName(postText)) {
+        document.getElementById("postText").focus();
+    } else {
         productsArray.unshift(
             {
                 bookName: postName,
@@ -207,11 +208,11 @@ function createBookPost() {
                 price: postPrice,
                 author: postAuthor,
                 genre: postGenre,
-                info: postText  
+                info: postText
             }
         );
     }
-    updateProductsList();   
+    updateProductsList();
 }
 
 //update products list when user adds a new book
@@ -227,49 +228,49 @@ function updateProductsList() {
 }
 
 // called when genre selection is changed
-function genreSelection(){
+function genreSelection() {
     var genreList = document.getElementById("postBookGenre");
     var genreName = genreList.options[genreList.selectedIndex].value;
     alert(genreName);
 }
 
 //regex to check book name is valid
-function checkName(postName){
+function checkName(postName) {
     //any Uppercase/Lowercase letters & numbers, between 1-50 characters
     var nameRegex = /^[A-Za-z\d]{1,50}$/;
     //get input from the user
-    if(postName !== "" && nameRegex.test(postName)){
+    if (postName !== "" && nameRegex.test(postName)) {
         return true;
     } else {
         return false;
     }
 }
 //regex to check link is to an image
-function checkImage(imageURL){
+function checkImage(imageURL) {
     //any Uppercase/Lowercase letters & numbers, between 6-10 characters
     var imageRegex = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/;
     //get input from the user
-    if(imageURL !== "" && imageRegex.test(imageURL)){
+    if (imageURL !== "" && imageRegex.test(imageURL)) {
         return true;
     } else {
         return false;
     }
 }
 //regex to check name format
-function checkAuthorName(authorName){
+function checkAuthorName(authorName) {
     //any Uppercase/Lowercase letters & numbers, between 6-10 characters
     var authorRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
     //get input from the user
-    if(authorName !== "" && authorRegex.test(authorName)){
+    if (authorName !== "" && authorRegex.test(authorName)) {
         return true;
     } else {
         return false;
     }
 }
 //regex to check price format
-function checkPrice(price){
+function checkPrice(price) {
     //get input from the user
-    if(price !== "" && !isNaN(price)){
+    if (price !== "" && !isNaN(price)) {
         return true;
     } else {
         return false;
@@ -278,8 +279,18 @@ function checkPrice(price){
 
 // Initialize and add the map
 function initMap() {
-    var bookShop = {lat: 53.33306, lng: -6.24889};
+    var bookShop = { lat: 53.33306, lng: -6.24889 };
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 15, center: bookShop});
-    var marker = new google.maps.Marker({position: bookShop, map: map});
+        document.getElementById('map'), { zoom: 15, center: bookShop });
+    var marker = new google.maps.Marker({ position: bookShop, map: map });
+}
+
+// Handle hiding/showing of the checkout form
+function openForm(object) {
+    currentButtonObject = object;
+    document.getElementById("checkoutForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("checkoutForm").style.display = "none";
   }
